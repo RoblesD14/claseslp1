baec20f2d1cfb528f182185dc24d256c9ae72910
MRobles <mnrdrobles@gmail.com>
2020-06-23 17:51:57 -0500

    clasesl

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$hola = "Index";
+$mensaje = "Hola Mundo";
+?>
+<! DOCTYPE html >
+< html  lang = " en " >
+< head >
+    < meta  charset = " UTF-8 " >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    < títle> <?php echo $hola; ?> </ títle >
+    </head>
+    <body>
+    <h1>
+
+    <?php echo $mensaje = 'Bienvenidos'; ?>
+    </h1>
+</body >
+</html >
