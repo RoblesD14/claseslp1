20fe44fe5247ca5f52a97c5bca738d3aa0f2e865
MRobles <mnrdrobles@gmail.com>
2020-07-05 20:39:21 -0500

    en clases

---
+++ operacion.php
@@ -0,0 +1,24 @@
+<? php
+
+clase  Operaciones {
+     función estática  pública suma ( $ nro1 , $ nro2 ): void { 
+        $ resultado = $ nro1 + $ nro2 ;
+        echo  "el resultado de la suma es:" . $ resultado ;
+    }
+
+     función pública estática  resta ( $ nro1 , $ nro2 ): void { 
+        $ resultado = $ nro1 - $ nro2 ;
+        echo  "el resultado de la resta es:" . $ resultado ;
+    }
+
+     multiplicación de la función estática  pública ( $ nro1 , $ nro2 ): void { 
+        $ resultado = $ nro1 * $ nro2 ;
+        echo  "el resultado de la multiplicación es:" . $ resultado ;
+    }
+
+     división de función estática  pública ( $ nro1 , $ nro2 ): void { 
+        $ resultado = $ nro1 / $ nro2 ;
+        echo  "el resultado de la división es:" . $ resultado ;
+    }
+
+}
